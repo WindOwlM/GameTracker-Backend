1c54ff3695c6fc87bbd0f790c264328fefb5d072
Maicol <mamian642@gmail.com>
2025-09-28 09:02:14 -0500

    Feature: se crea la coneccion a la base de datos y se prueba para su funcionamiento

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Conectado a MongoDB Atlas");
+  } catch (err) {
+    console.error("Error al conectar a MongoDB:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config/db");
+const app = require("./app")
+
+connectDB();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
