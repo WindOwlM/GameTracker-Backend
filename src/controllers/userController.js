fb7190d3bddc2f96d50cdb13244a919c5bb145fa
Maicol <mamian642@gmail.com>
2025-10-01 15:16:55 -0500

    Feature: se hace el update user y delete user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,29 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+const updateUser = async (req, res) => {
+  try {
+    const uuser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!uuser) return res.status(404).json({ error: "Usuario no encontrado" });
+    res.json(uuser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const dUser = await User.findByIdAndDelete(req.params.id);
+    if (!dUser) return res.status(404).json({ error: "Usuario no encontrado" });
+    res.json({ mensaje: "Usuario eliminado" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 module.exports = {
     createUser,
-    getAllUsers
+    getAllUsers,
+    updateUser,
+    deleteUser
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,14 @@ const express = require("express")
 const router = express.Router()
 const {
     createUser,
-    getAllUsers
+    getAllUsers,
+    updateUser,
+    deleteUser
 } = require("../controllers/userController")
 
 router.post("/",createUser)
 router.get("/",getAllUsers)
+router.patch("/:id",updateUser)
+router.delete("/:id",deleteUser)
 
 module.exports = router
