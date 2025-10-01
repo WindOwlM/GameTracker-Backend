6d559748233b85c9ffdd8365a39283310c614e8b
Maicol <mamian642@gmail.com>
2025-10-01 15:06:28 -0500

    Feature: se hace el obtener todos los usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,6 +10,16 @@ const createUser = async (req, res) => {
   }
 };
 
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 module.exports = {
-    createUser
+    createUser,
+    getAllUsers
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const {
-    createUser
+    createUser,
+    getAllUsers
 } = require("../controllers/userController")
 
 router.post("/",createUser)
+router.get("/",getAllUsers)
 
 module.exports = router
