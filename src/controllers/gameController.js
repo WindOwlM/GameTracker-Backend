2f1ab77b9b758556119b0626612f33909d927a71
Maicol <mamian642@gmail.com>
2025-10-21 17:38:33 -0500

    Fix: se borra el intento de comunicar con API, no esta permitido :c

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,53 +1,10 @@
-const fs = require("fs")
-
 const Game = require("../models/gameModel")
 
-const buscar = async (req,res) => {
-
+const createGame = async (req,res) => {
     try{
-        const data = fs.readFileSync("./game.json", "utf-8")
-        const games =JSON.parse(data)
-
-        const data2 = fs.readFileSync("./screenshots.json", "utf-8")
-        const pantalla =JSON.parse(data2)
-
-        const data3 = fs.readFileSync("./achievements.json", "utf-8")
-        const logros =JSON.parse(data3)
-
-        const plataformas = games.platforms
-        ? games.platforms.map(p => p.platform.name)
-        : [];
-
-        const generos = games.genres
-        ? games.genres.map(g => g.name)
-        : [];
-
-        const tienda = games.stores
-        ? games.stores.map(s => s.store.name)
-        : [];
-
-        const tags = games.tags
-        ? games.tags.map(t => t.name)
-        : [];
-
-        const result = {
-            id: games.id,
-            nombre: games.name,
-            plataformas,
-            released: games.released,
-            cover_image: games.background_image,
-            generos,
-            tienda,
-            tags,
-            pantallazos:{
-                cuenta:pantalla.count,
-                varios:pantalla.results.map(s => s.image)
-            },
-            logros:{
-                cuenta: logros.count
-            }
-        }
-        res.json(result);
+        const newGame = new Game(req.body)
+        await newGame.save()
+        res.status(201).json(newGame)
     }catch(err) {
         res.status(500).json({ error: err.message });
     }
@@ -55,5 +12,5 @@ const buscar = async (req,res) => {
 }
 
 module.exports ={
-    buscar
+    createGame
 }
--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const {
-    buscar
+    createGame
 } = require("../controllers/gameController")
 
 
-router.get("/",buscar)
+router.get("/",createGame)
 
 module.exports = router
